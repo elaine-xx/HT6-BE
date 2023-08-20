cb340b5ab497c211bcd91d3fb8b5905d1880676f
Lanh Hoang <lanhhoang@users.noreply.github.com>
2023-08-20 08:24:59 -0400

    open the image

--- conversation.py
+++ conversation.py
@@ -26,7 +26,7 @@ class Conversation(Resource):
       "daanelson/minigpt-4:b96a2f33cc8e4b0aa23eacfce731b9c41a7d9466d9ed4e167375587b54db9423",
       input={
         "prompt": history,
-        "image": image_url,
+        "image": open(image_url, 'rb'),
         "num_beams": 1,
         "temperature": 0.1
       }
